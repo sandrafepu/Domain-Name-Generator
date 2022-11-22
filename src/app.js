889bcdb44cf04c8e340da30577c6694f9365ac0f
sandrafepu <sandrafepu@gmail.com>
2022-11-22 10:23:29 +0000

    Subida de proyecto js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#the-domain").innerHTML = generateUrl();
+};
+
+let generateUrl = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".es"];
+
+  pronoun.forEach(element1 => {
+    adj.forEach(element2 => {
+      noun.forEach(element3 => {
+        extension.forEach(element4 => {
+          console.log(element1 + element2 + element3 + element4);
+        });
+      });
+    });
+  });
 };
